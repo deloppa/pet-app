837cfef773d4d1d9986c9b6a70609c870ba1bab8
deloppa <sveta.bakun03@gmail.com>
2025-04-18 00:06:35 +0300

    Lab 5: Add RBAC to API endpoints

---
+++ Dockerfile.app
@@ -0,0 +1,16 @@
+FROM maven:3.9.9-eclipse-temurin-21-alpine
+
+WORKDIR /app
+
+COPY pom.xml .
+
+RUN \
+    mvn dependency:go-offline -DskipTests
+
+COPY src src
+
+RUN \
+    mvn clean install
+
+ENTRYPOINT ["java", "-jar"]
+CMD ["./target/pet_app-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/pet/pet_app/config/SecurityConfig.java
+++ src/main/java/com/pet/pet_app/config/SecurityConfig.java
@@ -2,9 +2,16 @@ package com.pet.pet_app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,10 +19,50 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
   @Bean
+  public static PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+        .authorizeHttpRequests(
+            req ->
+                req.requestMatchers("index.html")
+                    .permitAll()
+                    .requestMatchers(HttpMethod.GET, "/api/v1/pets")
+                    .hasRole("USER")
+                    .requestMatchers("/api/v1/pets/**")
+                    .hasRole("ADMIN")
+                    .anyRequest()
+                    .authenticated())
         .httpBasic(Customizer.withDefaults());
     return http.build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    UserDetails superAdmin =
+        User.builder()
+            .username("super_admin")
+            .password(passwordEncoder().encode("super_admin"))
+            .roles("SUPER_ADMIN", "ADMIN", "USER")
+            .build();
+
+    UserDetails admin =
+        User.builder()
+            .username("admin")
+            .password(passwordEncoder().encode("admin"))
+            .roles("ADMIN", "USER")
+            .build();
+
+    UserDetails user =
+        User.builder()
+            .username("user")
+            .password(passwordEncoder().encode("user"))
+            .roles("USER")
+            .build();
+
+    return new InMemoryUserDetailsManager(superAdmin, admin, user);
   }
 }
